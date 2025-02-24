3f16211579274d4d346aeac392b035750715d7fe
gabrielevarao <gvarao93@gmail.com>
2025-02-24 17:46:17 -0300

    Inserção das funções da aplicação e do arquivo README.

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center"> Challenge Amigo Secreto </h1>
+<p align="center"> ONE - Oracle Next Education | Alura | Formação Lógica de Progamação</p> 
+<br>
+
+Nesta aplicação, você pode sortear um amigo secreto com base em uma lista de amigos.
+
+## :mag: Como funciona?
+Ao abrir o arquivo HTML no navegador, você verá esta página:
+>
+>
+1. Primeiramente, insira o nome das pessoas que participarão do Amigo Secreto. Escreva um nome por vez. Em seguida, clique no botão **Adicionar**. A cada pessoa adicionada, você verá a lista dos nomes inseridos até então. 
+2. Quando terminar que inserir o nome de todas as pessoas que participarão do Amigo Secreto, clique no botão **Sortear amigo**. Em seguida, o nome do seu amigo secreto será mostrado.
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigos = [];
+let listaAmigos = document.getElementById('listaAmigos');
+
+function adicionarAmigo(){
+
+    let nome = document.querySelector('input').value;
+    
+    if (nome === ''){
+        alert("Por favor, insira um nome.");
+
+    } else {
+        nomeAmigos.push(nome);
+        listaAmigos.innerHTML = '';
+
+        for (let i = 0; i < nomeAmigos.length; i++){
+            let li = document.createElement('li');
+            li.textContent = nomeAmigos[i];
+            listaAmigos.appendChild(li);
+        }
+
+        document.querySelector('input').value = '';
+    }
+}
+
+function sortearAmigo(){
+
+    if (nomeAmigos.length === 0){
+        alert("Por favor, insira um nome.");
+
+    } else {
+        let numSorteado = Math.floor(Math.random() * nomeAmigos.length);
+        let amigoSorteado = nomeAmigos[numSorteado];
+        let resultado = document.getElementById('resultado');
+        resultado.textContent = `A pessoa sorteada foi: ${amigoSorteado}.`;
+    }
+}
+
